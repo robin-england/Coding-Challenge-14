51ab60d8d3292c9e013b56b812a87b1f9f881807
robin-england <robin.england94@googlemail.com>
2024-06-24 20:14:51 -0400

    Create loan payment calculation module.

--- loan_payment.js
+++ loan_payment.js
@@ -1,2 +1,8 @@
 // U63166005
 // Coding Challenge #14
+
+// M = P[r(1+r)^n]/[(1+r)^n-1]
+
+export function loanPaymentCalc(principal, rate, time) {    // Assuming time in months
+    principal(rate( 1 + rate)^ time ) / (( 1 + rate ) ^ time-1 )
+}
--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 // Coding Challenge #14
 
 import { interestCalc } from "./interest_rate.js"; 
+import { loanPaymentCalc } from "./loan_payment.js"; 
+
