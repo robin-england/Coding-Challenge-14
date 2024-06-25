47bffdeeac873e3e4f0409b459cdf22641c2fd5f
robin-england <robin.england94@googlemail.com>
2024-06-24 20:11:49 -0400

    Implement interest rate calculation module.

--- interest_rate.js
+++ interest_rate.js
@@ -1,2 +1,8 @@
 // U63166005
 // Coding Challenge #14
+
+// Interest = Principal * Rate * Time
+
+export function interestCalc(principal, rate, time){    
+    principal*rate*time
+}
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // U63166005
 // Coding Challenge #14
 
+import { interestCalc } from "./interest_rate.js"; 
